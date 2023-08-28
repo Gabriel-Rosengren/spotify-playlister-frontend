f71a2eff7c4bb40ad0afb46ab218dac5791edb87
Gabriel Rosengren <gabbe.rosengren@gmail.com>
2023-08-28 11:05:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
 function App() {
+  const getPlaylists = () => {
+    axios.post('/playlist', {
+      user_id: 'gabbe1010',
+      limit: 10,
+      offset: 0
+    })
+    .then(res => {
+      console.log(res.data)
+    })
+  }
+
+  const getUser = () => {
+    axios.get('/user')
+    .then(res => {
+      console.log(res.data)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href='http://localhost:4000/login'>Log in</a>
+      <button onClick={getUser}>Get User</button>
+      <button onClick={getPlaylists}>Get Playlists</button>
     </div>
   );
 }
